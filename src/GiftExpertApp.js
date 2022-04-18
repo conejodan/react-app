07588278e796538d6f5e4218bbd4aff2f84d0077
Daniel Sanchez <conejodan@gmail.com>
2022-04-18 08:10:41 -0500

    Creando primer componente

---
+++ src/GiftExpertApp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class GiftExpertApp extends Component {
+    render() {
+        return (
+            <>
+                <h2>GiftExpertApp</h2>
+                <hr />
+            </>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GiftExpertApp from './GiftExpertApp';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  //<React.StrictMode>
+  //  <App />
+  //</React.StrictMode>,
+  <GiftExpertApp />,
   document.getElementById('root')
 );
 
