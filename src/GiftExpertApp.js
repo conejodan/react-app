47bf2a3d30e5d307b55ab4061ca0dae5bd8e8271
Daniel Sanchez <conejodan@gmail.com>
2022-04-18 08:14:08 -0500

    Agregando categorias

--- src/GiftExpertApp.js
+++ src/GiftExpertApp.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 
 export default class GiftExpertApp extends Component {
+    
+
     render() {
+        const categories = ['Dragon Ball', 'Naruto', 'One Piece'];
         return (
             <>
                 <h2>GiftExpertApp</h2>
                 <hr />
+                <ol>
+                    {categories.map(item => <li>{item}</li>)}
+                </ol>
             </>
         )
     }
