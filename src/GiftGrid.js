40a5e6f16ac3a46884004c5c6ab2d752d84e20ab
Daniel Sanchez <conejodan@gmail.com>
2022-04-19 21:41:29 -0500

    Implementando custom hooks

--- src/GiftGrid.js
+++ src/GiftGrid.js
@@ -1,17 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useFetchGifs } from './hooks/useFetchGifs';
+// import React, { useEffect, useState } from 'react';
 import { GiftGridItem } from './GiftGridItem';
-import { getGifs } from './helpers/getGifs';
 
 export const GiftGrid = ({ category }) => {
-    const [images, setImages] = useState([]);
 
-    useEffect(() => {
-        getGifs(category).then(setImages);
-    }, [category]);
+    const { data: images, loading } = useFetchGifs(category);
 
     return (
         <>
             <h3>{category}</h3>
+            {loading && <p>Cargando...</p>}
             <div className='card-grid'>
                 {images.map(item => {
                     return (
